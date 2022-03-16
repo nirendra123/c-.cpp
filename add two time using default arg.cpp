7a31d7abdc0e29efac014e0ccf4235dbd85e41f5
nirendra123 <91243848+nirendra123@users.noreply.github.com>
2022-03-16 08:39:17 +0545

    Update add two time using default arg.cpp

--- add two time using default arg.cpp
+++ add two time using default arg.cpp
@@ -1,1 +1,33 @@
-
+#include<iostream>
+using namespace std;
+class Time{
+	private:
+	int hour;
+	int minute;
+	int second;
+    void input(h,min=0,sec=0){
+    	hour=h;
+    	minute=min;
+    	second=sec;
+	}
+	Time calculation(Time test1,time test2){
+		
+		
+	}
+	void output(){
+		cout<<" THE EXACT TIME IS "<<hour<<":"<<minute<<":"<<sec;
+	}
+	
+};
+int main(){
+Time obj,obj1,obj2;
+int p,q,r;
+cout<<"Enter the Hour,Minute and second";
+cin>>p,
+obj1.input()
+	
+	
+	
+	
+	
+}
