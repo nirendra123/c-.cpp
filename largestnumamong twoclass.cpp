bf48d08c274cf50489b58a0dee653b60b001bcef
nirendra123 <91243848+nirendra123@users.noreply.github.com>
2022-03-16 15:01:20 +0545

    Create largestnumamong twoclass.cpp

---
+++ largestnumamong twoclass.cpp
@@ -0,0 +1,14 @@
+//wap to find largest number among two class
+#include<iostream>
+#define PI 3.14;
+class Operation{
+	private:
+		int length;
+		int breadth;
+		int radius;
+		public:
+			friend
+			
+};
+class Operation2{
+};
