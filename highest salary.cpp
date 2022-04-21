f04a1b36ff1b5493553d2e52e5230deef3ec5c27
nirendra123 <91243848+nirendra123@users.noreply.github.com>
2022-04-21 14:20:00 +0545

    Create highest salary.cpp

---
+++ highest salary.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class employee{
+	private:
+	 string name;
+	 int id;
+	 int salary;
+	 public:
+	 	void setinput(){
+	 		cout>>"enter the name of employee:";
+	 		cin<<name;
+	 		cout>>"enter the id of emmployee:";
+	 		cin>>id;
+	 		cout<<"enter the salary of employee";
+	 		cin>>salary;
+		 }
+		  void compare()
+		  { if()
+		  
+		}
+		 void output{
+		 	
+		 cout<<" name:"<<name;
+		 cout<<"id:"<<id;
+		 cout<<"salary"<<salary;
+		 
+		 }
+};
